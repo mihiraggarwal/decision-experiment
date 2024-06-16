ca38e659136ae77f422a581526ad4d48a0a4c192
Mihir Aggarwal <49950982+mihiraggarwal@users.noreply.github.com>
2024-06-16 13:47:09 +0530

    add verification rule

--- src/app/api/auth/verify/route.ts
+++ src/app/api/auth/verify/route.ts
@@ -3,16 +3,11 @@ import User from "@/app/_models/User";
 import { NextRequest, NextResponse } from "next/server";
 
 export const POST = async (req: NextRequest) => {
-    console.log("function started")
     const body = await req.json()
-    console.log("function running")
     await dbConnect()
-    console.log("db also connected")
     try {
         const existing = await User.findOne({ upi: body.upi })
-        console.log("existing", existing)
         if (existing != null) {
-            console.log("in existing")
             return NextResponse.json({
                 error: "User already exists",
             }, {
@@ -20,16 +15,13 @@ export const POST = async (req: NextRequest) => {
             })
         }
     } catch (e) {
-        console.log("user find error", e)
+        console.log("error finding user:", e)
     }
 
-    console.log("reached after")
     const user = await User.findOne({
         password: body.otp,
     });
-    console.log("user", user)
     if (user == null) {
-        console.log("in user null")
         return NextResponse.json({
             error: "Incorrect password",
         }, {
@@ -37,7 +29,14 @@ export const POST = async (req: NextRequest) => {
         })
     }
 
-    console.log("final")
+    if (user.upi != null) {
+        return NextResponse.json({
+            error: "User already exists",
+        }, {
+            status: 401
+        })
+    }
+
     return NextResponse.json({}, {
         status: 200
     })
