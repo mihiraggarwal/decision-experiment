6b59ad0defab429da97e9ea88f3fa69595fc8de7
Mihir Aggarwal <49950982+mihiraggarwal@users.noreply.github.com>
2024-06-16 12:52:42 +0530

    try catch on error point

--- src/app/api/auth/verify/route.ts
+++ src/app/api/auth/verify/route.ts
@@ -5,15 +5,19 @@ export const POST = async (req: NextRequest) => {
     console.log("function started")
     const body = await req.json()
     console.log("function running")
-    const existing = await User.findOne({ upi: body.upi })
-    console.log("existing", existing)
-    if (existing != null) {
-        console.log("in existing")
-        return NextResponse.json({
-            error: "User already exists",
-        }, {
-            status: 401
-        })
+    try {
+        const existing = await User.findOne({ upi: body.upi })
+        console.log("existing", existing)
+        if (existing != null) {
+            console.log("in existing")
+            return NextResponse.json({
+                error: "User already exists",
+            }, {
+                status: 401
+            })
+        }
+    } catch (e) {
+        console.log("user find error", e)
     }
 
     console.log("reached after")
