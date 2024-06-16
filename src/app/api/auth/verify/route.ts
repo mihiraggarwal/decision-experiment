ae2ec20d822a247c35cf8d79476079eb1ad9c961
Mihir Aggarwal <49950982+mihiraggarwal@users.noreply.github.com>
2024-06-16 12:36:47 +0530

    vercel error diagnosis

--- src/app/api/auth/verify/route.ts
+++ src/app/api/auth/verify/route.ts
@@ -2,29 +2,36 @@ import User from "@/app/_models/User";
 import { NextRequest, NextResponse } from "next/server";
 
 export const POST = async (req: NextRequest) => {
+    console.log("function started")
     const body = await req.json()
-
+    console.log("function running")
     const existing = await User.findOne({ upi: body.upi })
-        if (existing != null) {
-            return NextResponse.json({
-                error: "User already exists",
-            }, {
-                status: 401
-            })
-        }
-
-        const user = await User.findOne({
-            password: body.otp,
-        });
-        if (user == null) {
-            return NextResponse.json({
-                error: "Incorrect password",
-            }, {
-                status: 401
-            })
-        }
-
-        return NextResponse.json({}, {
-            status: 200
+    console.log("existing", existing)
+    if (existing != null) {
+        console.log("in existing")
+        return NextResponse.json({
+            error: "User already exists",
+        }, {
+            status: 401
         })
+    }
+
+    console.log("reached after")
+    const user = await User.findOne({
+        password: body.otp,
+    });
+    console.log("user", user)
+    if (user == null) {
+        console.log("in user null")
+        return NextResponse.json({
+            error: "Incorrect password",
+        }, {
+            status: 401
+        })
+    }
+
+    console.log("final")
+    return NextResponse.json({}, {
+        status: 200
+    })
 }
