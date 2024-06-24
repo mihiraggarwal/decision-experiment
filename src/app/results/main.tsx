57aea87fd158506711273bb9292f9dce3beec419
Mihir Aggarwal <49950982+mihiraggarwal@users.noreply.github.com>
2024-06-25 00:04:04 +0530

    show all bets on results

--- src/app/results/main.tsx
+++ src/app/results/main.tsx
@@ -1,29 +1,35 @@
 "use client"
 
+import { useState } from "react"
 import SubmitBtn from "../_components/submitBtn"
 
-export default function Main({chosen_bet, pdf_pass, bet, num, proceed, mcq, colours, balls, rewards}: {chosen_bet: number, pdf_pass: string, bet: number, num: number, proceed: () => void, mcq: boolean, colours: string[], balls: number[][], rewards: number[]}) {
+export default function Main({chosen_bet, pdf_pass, bet, proceed, mcq, rewards, all_colours, all_balls, all_rewards, all_responses, server_url, save_price, check_price}: {chosen_bet: number, pdf_pass: string, bet: number, proceed: () => void, mcq: boolean, rewards: number[], all_colours: string[][], all_balls: number[][][], all_rewards: number[][], all_responses: number[], server_url: string, save_price: (num: number) => void, check_price: () => Promise<any>}) {
 
-    const Table = () => {
+    const [loading, setLoading] = useState(false)
+    const [further, setFurther] = useState(false)
+    const [num, setNum] = useState(0)
+    const [disable, setDisable] = useState(false)
+
+    const Table = ({num}: {num: number}) => {
         return (
             <table>
                 <tbody>
                     <tr>
                         <td></td>
-                        {balls.map((ball, index) => (
+                        {all_balls[num].map((ball, index) => (
                             <th colSpan={ball[0]} key={index}>{`${ball[1]} ball${ball[1] > 1 ? "s" : ""}`}</th>
                         ))}
                     </tr>
                     <tr>
                         <td></td>
-                        {colours.map((colour, index) => (
+                        {all_colours[num].map((colour, index) => (
                             <th key={index}>{colour}</th>
                             
                         ))}
                     </tr>
                     <tr>
-                        <th>Rewards</th>
-                        {rewards.map((reward, index) => (
+                        <th>Decision {num+1}</th>
+                        {all_rewards[num].map((reward, index) => (
                             <td key={index}>{`${reward}`}</td>
                         ))}
                     </tr>
@@ -32,30 +38,71 @@ export default function Main({chosen_bet, pdf_pass, bet, num, proceed, mcq, colo
         )
     }
 
+    const genRandom = async (rewards: number[]) => {
+        setLoading(true)
+        setDisable(true)
+        const f = await check_price()
+        if (f == -1) {
+        
+            const res = await fetch(`${server_url}/price`, {
+                method: "POST",
+                headers: {
+                    "Content-Type": "application/json"
+                },
+                body: JSON.stringify({
+                    rewards: rewards
+                })
+            })
+            const body = await res.json()
+            await save_price(body.price)
+            setNum(body.price)
+        } else {
+            setNum(f)
+        }
+        setLoading(false)
+        setFurther(true)
+    }
+
     return (
         <main className="flex min-h-screen flex-col items-center gap-20 p-24 pt-16">
-            <h1 className="text-3xl">Results</h1>
-            <div className="flex flex-col gap-5 items-center">
-                <p>The decision to be used for payment is: {chosen_bet}</p>
-                <p>You can verify this using the following password on your PDF: {pdf_pass}</p>
-                <Table />
+            <h1 className="text-3xl">Payments</h1>
+            <div className="flex flex-col gap-14">
+                <p>We will now determine your payment based on your responses to the choice problems earlier. Here is a list of all the bets that you evaluated during the experiment.</p>
 
-                {mcq ? (
-                    <p>Since the question was an MCQ, your bet will be carried out.</p>
-                ) : (
-                    <>
-                        <p>Your price for selling the bet was: {bet}</p>
-                        <p>The price the buyer is billing to buy at is: {num}</p>
-                    </>
-                )}
+                <div className="flex flex-col gap-14 items-start">
+                    {all_rewards.map((balls, index) => (
+                        <div className="flex flex-col gap-5" key={index}>
+                            <Table num={index} key={index} />
+                            <p>Your valuation of this bet was: {all_responses[index]}</p>
+                        </div>
+                    ))}
+                </div>
 
-                
-                {!mcq && num >= bet && (
-                    <p>The buyer&apos;s price is greater than your price, and so your bet was sold. Congratulations!</p>
-                )}
-                {!mcq && num < bet && (
-                    <p>The buyer&apos;s price is less than your price, and so you will now get to play the above bet.</p>
-                )}
+                <div className="flex flex-col gap-3 items-start">
+                    <p>Decision {chosen_bet} is to be used for payment. You can verify this using the following password on your PDF: {pdf_pass}.</p> 
+                    <p>Your selling price for the corresponding bet was {bet}. A number will now be picked at random to determine whether your bet will be sold</p>
+
+                    <button onClick={() => genRandom(rewards)} className={`border border-black rounded-md py-2 px-5 ${loading ? "bg-gray-300" : "bg-white"}`} disabled={disable}>{loading ? "Loading..." : "Generate"}</button>
+
+                    {further && (
+                        <>
+                            {mcq ? (
+                                <p>Since the question was an MCQ, your bet will be carried out.</p>
+                            ) : (
+                                <>
+                                    <p>The number generated is {num}</p>
+                                </>
+                            )}
+
+                            {!mcq && num >= bet && (
+                                <p>Congratulations! Your bet was sold for INR {num}.</p>
+                            )}
+                            {!mcq && num < bet && (
+                                <p>The generated number is lower than your selling price. You can now play the bet to determine your rewards.</p>
+                            )}
+                        </>
+                    )}
+                </div>
 
             </div>
             <form action={proceed}>
--- src/app/results/page.tsx
+++ src/app/results/page.tsx
@@ -2,6 +2,7 @@ import Main from "./main"
 
 import { getServerSession } from "next-auth";
 import User from "../_models/User";
+import Response from "../_models/Response";
 import dbConnect from "../_config/db";
 import navigate from "../_actions/navigate";
 
@@ -9,53 +10,125 @@ import getChosenBet from "../_actions/getChosenBet";
 import { saveToDB } from "../bet/play";
 import details from "../_actions/getTableDetails";
 
+import { cp1_bet1, cp1_bet2, cp1_bet3, cp1_bet4, cp2_bet1, cp2_bet2, cp2_bet3, cp2_bet4, cp3_bet1, cp3_bet2, cp4_bet1, cp4_bet2, cp4_bet3 } from "../_assets/bets";
+
 const RANDOMIZATION_SERVER_URL = process.env.RANDOMIZATION_SERVER_URL!
 
-const genRandom = async (rewards: number[]) => {
-    const res = await fetch(`${RANDOMIZATION_SERVER_URL}/price`, {
-        method: "POST",
-        headers: {
-            "Content-Type": "application/json"
-        },
-        body: JSON.stringify({
-            rewards: rewards
-        })
-    })
-    const body = await res.json()
-    return body.price
-}
-
 const savePrice = async (num: number) => {
+    "use server"
     const session = await getServerSession()
     await User.findOneAndUpdate({ password: session?.user.name }, { price: num })
+}
+
+const checkForPrice = async() => {
+    "use server"
+    const session = await getServerSession();
+    const user = await User.findOne({ password: session?.user.name })
+
+    const price = user.price
+    if (price != null) return price
+    return -1
 }
 
 export default async function Results() {
 
     await dbConnect()
 
-    const { chosen_bet, pdf_pass, bet, challenge, rewards, price } = await getChosenBet()
+    const session = await getServerSession();
+    const user = await User.findOne({ password: session?.user.name })
 
-    const mcq = Number(challenge.part!) >= 6
-    
-    let num;
-    if (mcq) num = -1
-    else {
-        if (!price) {
-            num = await genRandom(rewards)
-            await savePrice(num)
-        }
-        else {
-            num = price
+    const chosen_bet = user.chosen_bet
+    const pdf_pass = user.pdf_pass
+    const order = user.order
+
+    const answer = await Response.findOne({ session_id: user.id })
+    const response = answer.response
+
+    const all_colours = []
+    const all_balls = []
+    const all_rewards = []
+    const all_responses = []
+
+    for (let i = 0; i < order.length; i++) {
+        for (let j = 0; j < order[i].bets_order.length; j++) {
+            const { colours, balls } = await details({cp: order[i].cp, part: order[i].part, bet: order[i].bets_order[j]})
+            all_colours.push(colours)
+            all_balls.push(balls)
+            all_responses.push(response[i].answer[j])
+
+            let rewards: number[] = []
+
+            switch (order[i].cp) {
+                case 1:
+                    switch (order[i].bets_order[j]) {
+                        case 1:
+                            rewards = cp1_bet1
+                            break;
+                        case 2:
+                            rewards = cp1_bet2
+                            break;
+                        case 3:
+                            rewards = cp1_bet3
+                            break;
+                        case 4:
+                            rewards = cp1_bet4
+                            break;
+                    }
+                    break;
+                case 2:
+                    switch (order[i].bets_order[j]) {
+                        case 1:
+                            rewards = cp2_bet1
+                            break;
+                        case 2:
+                            rewards = cp2_bet2
+                            break;
+                        case 3:
+                            rewards = cp2_bet3
+                            break;
+                        case 4:
+                            rewards = cp2_bet4
+                            break;
+                    }
+                    break;
+                case 3:
+                    switch (order[i].bets_order[j]) {
+                        case 1:
+                            rewards = cp3_bet1
+                            break;
+                        case 2:
+                            rewards = cp3_bet2
+                            break;
+                    }
+                    break;
+                case 4:
+                    switch (order[i].bets_order[j]) {
+                        case 1:
+                            rewards = cp4_bet1
+                            break;
+                        case 2:
+                            rewards = cp4_bet2
+                            break;
+                        case 3:
+                            rewards = cp4_bet3
+                            break;
+                    }
+                    break;
+            }
+            all_rewards.push(rewards)
         }
     }
 
-    const { colours, balls } = await details(challenge)
+    const { bet, challenge, rewards } = await getChosenBet()
 
-    const session = await getServerSession()
+    const mcq = Number(challenge.part!) >= 6
 
     const proceed = async () => {
         "use server"
+
+        const user = await User.findOne({ password: session?.user.name })
+        const num = user.price
+
         if (num >= bet) {
             await User.findOneAndUpdate({ password: session?.user.name }, { fin: true, amount_bet: num, $inc : { total_amount: num } })
             await navigate("/fin")
@@ -66,6 +139,6 @@ export default async function Results() {
     }
 
     return (
-        <Main chosen_bet={chosen_bet} pdf_pass={pdf_pass} bet={bet} num={num} proceed={proceed} mcq={mcq} colours={colours} balls={balls} rewards={rewards} />
+        <Main chosen_bet={chosen_bet} pdf_pass={pdf_pass} bet={bet} proceed={proceed} mcq={mcq} rewards={rewards} all_colours={all_colours} all_balls={all_balls} all_rewards={all_rewards} all_responses={all_responses} server_url={RANDOMIZATION_SERVER_URL} save_price={savePrice} check_price={checkForPrice} />
     )
 }
