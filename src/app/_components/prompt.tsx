1365c76001f5e87196f8057a7e94ee20649123eb
Mihir Aggarwal <49950982+mihiraggarwal@users.noreply.github.com>
2024-06-25 03:39:56 +0530

    change prompt text

--- src/app/_components/prompt.tsx
+++ src/app/_components/prompt.tsx
@@ -1,9 +1,9 @@
 export default function Prompt({plural, radio}: {plural: boolean, radio: boolean}) {
     if (!radio) {
         if (plural == true) {
-            return (<p className="text-left w-full">You have a choice of either playing out the ticket you were offered and receiving the reward corresponding to the ball that is drawn or selling the ticket for a sure amount. What is the lowest amount at which you would be willing to sell these tickets?</p>)
+            return (<p className="text-left w-full">Suppose you could sell the ticket you were offered instead of playing out the bet. What is the lowest amount at which you would be willing to sell these tickets?</p>)
         } else {
-            return (<p className="text-left w-full">You have a choice of either playing out the ticket and receiving the reward corresponding to the ball that is drawn or selling the ticket for a sure amount. What is the lowest amount at which you would be willing to sell this ticket?</p>)
+            return (<p className="text-left w-full">Suppose you could sell the ticket instead of playing out the bet. What is the lowest price at which you would be willing to sell this ticket?</p>)
         }
     }
     return (<p className="text-left w-full">Which of these tickets would you choose to take?</p>)
