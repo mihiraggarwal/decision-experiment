193281f4a8e10328a70937026da6f95a23b86d2a
Mihir Aggarwal <49950982+mihiraggarwal@users.noreply.github.com>
2024-06-27 20:57:26 +0530

    remove maintenance

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,12 +1,12 @@
-// export { default } from "next-auth/middleware"
+export { default } from "next-auth/middleware"
 
-// export const config = {
-//     matcher: ["/((?!fin).+)"]
-// }
-
-import { NextFetchEvent, NextRequest, NextResponse } from "next/server";
-const MAINTENANCE = process.env.MAINTENANCE
-
-export function middleware(request: NextRequest, event: NextFetchEvent) {
-    if (MAINTENANCE == "true") return NextResponse.redirect(new URL("/maintenance", request.url));
+export const config = {
+    matcher: ["/((?!fin).+)"]
 }
+
+// import { NextFetchEvent, NextRequest, NextResponse } from "next/server";
+// const MAINTENANCE = process.env.MAINTENANCE
+
+// export function middleware(request: NextRequest, event: NextFetchEvent) {
+//     if (MAINTENANCE == "true") return NextResponse.redirect(new URL("/maintenance", request.url));
+// }
